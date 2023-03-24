86eaeba9f3c5268ae403a4c3f96932261287a4ac
Michael E <escribano.michael.me@gmail.com>
2023-03-23 22:10:53 -0500

    week 3 homework

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,15 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){("button!")
+var character = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var number ="0123456789"
+var passwordLength = 8;
+ var password = "";
+ for (var i = 0; i <= passwordLength; i++)
+  return "generated password will go here"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
